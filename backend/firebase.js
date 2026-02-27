1bb7685e53dc599fa38b0113f13629b2a01704d1
Nikita Essine <54572118+nikitaessine@users.noreply.github.com>
2026-02-27 09:47:10 +0200

    firebase credentialit siiretty .env ja node moduulit lisätty gitignoreen

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
+import "dotenv/config";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBxbxx6hfbQ4Mw1b71sKEOttjAR6yHONqo",
-  authDomain: "teks4407.firebaseapp.com",
-  databaseURL: "https://teks4407-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "teks4407",
-  storageBucket: "teks4407.firebasestorage.app",
-  messagingSenderId: "71543363715",
-  appId: "1:71543363715:web:743546d674f84cc14263b6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxbxx6hfbQ4Mw1b71sKEOttjAR6yHONqo',
-  authDomain: 'teks4407.firebaseapp.com',
-  databaseURL: 'https://teks4407-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'teks4407',
-  storageBucket: 'teks4407.firebasestorage.app',
-  messagingSenderId: '71543363715',
-  appId: '1:71543363715:web:743546d674f84cc14263b6',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
